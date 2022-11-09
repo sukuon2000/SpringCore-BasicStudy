162103cd0b54bfda8513ded2957baf511650ff69
sukuon2000 <kaiosuku@naver.com>
2022-11-09 22:24:57 +0900

    2022 11 09 study commit

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,57 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        //System.out.println("memberService = " + memberService);
+        //System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        // MemberService가 MemberServiceImpl의 인스턴스이면 성공
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class); // 타입으로만
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    // 구현을 직접 넣는 것은 좋은 코드가 아님
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+        //ac.getBean("xxxxx", MemberService.class);
+        MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+
+        // ac.getBean("xxxxx", MemberService.class)를 실행했을 때
+        // 던져준 Exception (NoSuchBeanDefinitionException) 이 터져야 성공
+        // 이 곳 Assertions는 junit의 것
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
